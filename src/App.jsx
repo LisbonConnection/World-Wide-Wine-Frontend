fb8e68935dae81c7f2767bfef6dced926101a832
Samuel Onoja <samuelonoja0@gmail.com>
2025-03-05 17:08:10 -0800

    pages and components setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import SignUp from "./pages/SignUp"
+import Navbar from "./components/Navbar";
+
 function App() {
 
   return (
     <>
-      <div>
-        <h1>Frontend</h1>
+    <div>
+
+      <Navbar/>
+      
+      <Routes>
+        <Route path="/" element = {<Home/>}/>
+        <Route path="/login" element = {<Login/>}/>
+        <Route path="/signup" element = {<SignUp/>}/>
+      </Routes>
+
       </div>
     </>
   )
