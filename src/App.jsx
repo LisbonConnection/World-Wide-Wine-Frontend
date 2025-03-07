1813373b7084d3215046b0a9ede397b77bc94951
Samuel Onoja <samuelonoja0@gmail.com>
2025-03-07 03:25:08 -0800

    implement winecard/details component

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import IsPrivate from "./components/IsPrivate";
 import IsAnon from "./components/IsAnon";
 import WineCard from "./components/WineCard";
 import ErrorPage404 from "./components/ErrorPage404";
+import WineDetails from "./components/WineDetails";
 
 
 // import { createContext } from "react";
@@ -34,6 +35,7 @@ function App() {
             <Route path="/allcollections" element={ <IsAnon> <AllCollectionWines /> </IsAnon>} />
             <Route path="/newcollections" element={ <IsAnon> <NewCollectionWines /> </IsAnon>} />
             <Route path="/bestratingns" element={ <IsAnon> <BestRatingWines /> </IsAnon>} />
+            <Route path="/wine/:id" element={<WineDetails/>}/>
             <Route path="*" element={<ErrorPage404 />} />
           </Routes>
         </div>
--- src/components/WineCard.jsx
+++ src/components/WineCard.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
-function WineCard() {
+function WineCard({ wine, onWineClick }) {
+  // Checking if ratingAverage exists and is a valid number
+  const rating = wine.ratingAverage && !isNaN(wine.ratingAverage) ? wine.ratingAverage.toFixed(2) : "No rating available";
+
   return (
-    <div>This is the WineCard</div>
-  )
+    <div className="wine-card" onClick={() => onWineClick(wine)} style={{ cursor: "pointer" }}>
+      <h3>{wine.wineName}</h3>
+      <p><strong>Varietal:</strong> {wine.varietalName}</p>
+      <p><strong>Region:</strong> {wine.region}</p>
+      <p><strong>Price:</strong> €{wine.price}</p>
+      <p><strong>Rating:</strong> {rating}</p> 
+    </div>
+  );
 }
 
-export default WineCard
+export default WineCard;
