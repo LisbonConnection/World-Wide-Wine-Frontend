2c627635b1f9ee2f9fd9e5d73cb613f3fcdf580b
Samuel Onoja <samuelonoja0@gmail.com>
2025-03-11 15:49:37 -0700

    add profile component layout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,113 +1,73 @@
 import React from "react";
-// import profilepic from "../assets/profilepic.jpg";
-// import githubb from "../assets/githubb.png";
-// import html from "../assets/html.png";
-// import tailwind from "../assets/tailwind.png";
-// import javascrp from "../assets/javascript.png";
-// import ract from "../assets/react.png";
-// import fbase from "../assets/firebase.png";
-// import { SocialIcon } from "react-social-icons";
-// import Telmo from "../assets/Telmo1.jpg";
+import { SocialIcon } from "react-social-icons";
+import Sam from "../assets/samshort.png";
 
 function About() {
-  //  const [isClicked, setisClicked] = useState(false);
-
-  // yesisClicked = () => {
-  //   setisClicked(isClicked)
-  // }
-
   return (
     <>
-      <section className="border-none shadow-none outline-none overflow-hidden">
-        <h1 className="mt-16 text-center tracking-widest text-gray-600 font-bold text-4xl">
-          About our Project
-        </h1>
-        <div>
-          {/* <p className="tracking-widest text-gray-500 font-medium text-center mt-5">
+      <div className="flex min-h-full">
+        <div className=" flex space-y-10 bg-white shadow-2xl rounded-2xl"></div>
 
-          </p>
-        </div>
-        <div className=" mt-20 flex items-center justify-center min-h-full border-none shadow-none outline-none overflow-hidden">
-          <div className=" flex-col items-center m-6 space-y-10 shadow-2xl outline-none rounded md:flex flex-row md:space-y-0 md:m-0">
-            first profile
-            <div className="flex justify-center rounded-2xl border-2 border-y-blue-600 border-x-gray-200 h-90 w-80 items-center justify-between md:p-20">
-              <div>
-                <div className="flex items-center justify-center">
-                  <img
-                    src={profilepic}
-                    alt="profile-pic"
-                    className="h-35 w-35 items-center mb-10 rounded-full"
-                  />
-                </div>
-                <div>
-                  <h1 className="text-center text-xl text-gray-600 font-bold">
-                    Samuel Onoja
-                  </h1>
-                </div>
-
-                <div className="flex space-x-10 mt-5">
-                  <div>
-                    <SocialIcon url="https://github.com/Samuelonoja" />
-                  </div>
-                  <div>
-                    <SocialIcon url="https://www.linkedin.com/in/samuelonoja" />
-                  </div>
-                </div>
-              </div>
+        {/* Left */}
+        <div className="p-6 bg-white w-1/2 flex flex-col justify-center items-center">
+          <div className="flex flex-col ">
+            <div className="flex items-center justify-center">
+              <img
+                src={Sam}
+                alt="profile-pic"
+                className="w-2/4 h-80 object-cover rounded-lg mb-2 duration-200 hover:scale-110"
+              />
             </div>
 
-            second profile
-            <div className="flex justify-center rounded-2xl border-2 border-y-blue-300 border-x-gray-400 h-90 w-80 items-center justify-between md:p-20">
-              <div>
-                <div className="flex items-center justify-center">
-                  <img src={Telmo} alt="Profile Picture" className="h-35 w-35 items-center mb-10 rounded-full"/>                    
+            <h1 className="mt-5 text-2xl text-center text-gray-600 font-bold">
+              Samuel Onoja
+            </h1>
+
+            <h1 className="mt-5">
+              A passionate Full Stack MERN Developer with expertise in building
+              dynamic and scalable web applications. I specialize in the MERN
+              stack, which comprises MongoDB, Express.js, React, and Node.js,
+              and I also have experience building cross-platform mobile
+              applications with Flutter.
+            </h1>
+
+            <div className="mt-10 text-2xl flex justify-center">
+              <div className="flex space-x-10">
+                <div>
+                  <SocialIcon url="https://github.com/Samuelonoja" />
                 </div>
                 <div>
-                  <h1 className="text-center text-xl text-gray-600 font-bold">
-                    Telmo Lousada
-                  </h1>
-                </div>
-
-                <div className="flex space-x-10 mt-5">
-                  <div>
-                    <SocialIcon url="https://github.com/TelmoLousada" />
-                  </div>
-                  <div>
-                    <SocialIcon url="https://www.linkedin.com/in/telmo-lousada/" />
-                  </div>
+                  <SocialIcon url="https://www.linkedin.com/in/samuelonoja" />
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="text-center text-2xl text-gray-600 font-bold mt-20">
-          <h1>Tools Used</h1>
+
+        {/* right */}
+        <div className="p-6 bg-gray-50 w-1/2 flex flex-col justify-center items-center">
+          <div className="flex flex-col ">
+            <div className="flex items-center justify-center">IMG</div>
+
+            <h1 className="mt-5 text-2xl text-center text-gray-600 font-bold">
+              Name
+            </h1>
+
+            <h1 className="mt-5">About</h1>
+
+            <div className="mt-10 text-2xl flex justify-center">
+              <div className="flex space-x-10">
+                <div>
+                  <SocialIcon url="https://github.com/" />
+                </div>
+                <div>
+                  <SocialIcon url="https://www.linkedin.com/in/" />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col md:flex-row space-x-5 mt-5 items-center justify-center mb-10">
-          <div>
-            <img src={javascrp} alt="img" className="h-10 w-10 rounded-full" />
-          </div>
-          <div>
-            <img src={ract} alt="img" className="h-10 w-10 rounded-full" />
-          </div>
-          <div>
-            <img src={html} alt="img" className="h-10 w-10 rounded-full" />
-          </div>
-          <div>
-            <img src={tailwind} alt="img" className="h-10 w-10 rounded-full" />
-          </div>
-          <div>
-            <img src={fbase} alt="img" className="h-10 w-10 rounded-full" />
-          </div>
-          <div className="bg-black rounded-full">
-            <img
-              src={githubb}
-              alt="img"
-              className="h-10 w-10 rounded-full cover"
-            />
-          </div> */}
-        </div>
-      </section>
+      </div>
     </>
   );
 }
