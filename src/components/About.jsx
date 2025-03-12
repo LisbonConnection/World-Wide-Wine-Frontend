9037e8ac3e3d269674f8ae17612a80a3f73c243d
Telmo Lousada <telmo.lousada@gmail.com>
2025-03-12 06:43:00 +0000

    update about page bio and icons

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
 import Sam from "../assets/samshort.png";
+import Telmo from "../assets/Telmo1.jpg";
 
 function About() {
   return (
@@ -34,10 +35,14 @@ function About() {
             <div className="mt-10 text-2xl flex justify-center">
               <div className="flex space-x-10">
                 <div>
-                  <SocialIcon url="https://github.com/Samuelonoja" />
+                  <SocialIcon url="https://github.com/Samuelonoja" 
+                  className="w-4/2 h-80 object-cover rounded-lg mb-2 duration-200 hover:scale-110"
+                  />
                 </div>
                 <div>
-                  <SocialIcon url="https://www.linkedin.com/in/samuelonoja" />
+                  <SocialIcon url="https://www.linkedin.com/in/samuelonoja" 
+                  className="w-4/2 h-80 object-cover rounded-lg mb-2 duration-200 hover:scale-110"
+                  />
                 </div>
               </div>
             </div>
@@ -45,23 +50,37 @@ function About() {
         </div>
 
         {/* right */}
-        <div className="p-6 bg-gray-50 w-1/2 flex flex-col justify-center items-center">
+        <div className="p-6 bg-white w-1/2 flex flex-col justify-center items-center">
           <div className="flex flex-col ">
-            <div className="flex items-center justify-center">IMG</div>
+            <div className="flex items-center justify-center">
+              <img
+                src={Telmo}
+                alt="profile-pic"
+                className="w-2/4 h-80 object-cover rounded-lg mb-2 duration-200 hover:scale-110"
+              />
+            </div>
 
             <h1 className="mt-5 text-2xl text-center text-gray-600 font-bold">
-              Name
+              Telmo Lousada
             </h1>
 
-            <h1 className="mt-5">About</h1>
+            <h1 className="mt-5">
+            A dedicated full-stack developer with growing MERN and JavaScript expertise.
+            With over a decade in senior retail and people management, I bring strong leadership and problem-solving skills to tech.
+            Passionate about building user-friendly applications, I thrive in collaborative environments and embrace new challenges.
+            </h1>
 
             <div className="mt-10 text-2xl flex justify-center">
               <div className="flex space-x-10">
                 <div>
-                  <SocialIcon url="https://github.com/" />
+                  <SocialIcon url="https://github.com/TelmoLousada" 
+                  className="w-4/2 h-80 object-cover rounded-lg mb-2 duration-200 hover:scale-110"
+                  />
                 </div>
                 <div>
-                  <SocialIcon url="https://www.linkedin.com/in/" />
+                  <SocialIcon url="https://www.linkedin.com/in/telmo-lousada/" 
+                  className="w-4/2 h-80 object-cover rounded-lg mb-2 duration-200 hover:scale-110"
+                  />
                 </div>
               </div>
             </div>
