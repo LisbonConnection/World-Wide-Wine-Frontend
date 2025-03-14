4cfb77d7c1218a805a9ad1620501f51db8859c6a
Telmo Lousada <telmo.lousada@gmail.com>
2025-03-14 11:26:45 +0000

    reverse changes made in previous commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,14 +6,12 @@ import axios from "axios";
 import { FaPlus } from "react-icons/fa";
 import { FaInfoCircle } from "react-icons/fa";
 import { useEffect } from "react";
-import WWW2025 from "../assets/WWW2025.png";
 
 function Navbar() {
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
   const [text, setText] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const location = useLocation();
-  const [isCollectionOpen, setIsCollectionOpen] = useState(false);
 
   const handleChange = (e) => {
     setText(e.target.value);
@@ -102,11 +100,10 @@ function Navbar() {
           <div className="flex space-x-20">
             <Link to={isLoggedIn ? "/dashboard" : "/"} onClick={handleNavigate}>
               <div className="flex flex-col ml-5">
-                <img 
-                src={WWW2025} 
-                alt="World Wide Wine Logo" 
-                className="w-50 h-auto" 
-              />
+                <p className="w-auto text-xl font-bold flex justify-cente text-blue-950">
+                  WWW
+                </p>
+                <p className="w-auto italic text-sm text-blue-950">World Wide Wine</p>
               </div>
             </Link>
 
@@ -136,47 +133,41 @@ function Navbar() {
               <>
                 <div className="flex">
                   <ul className="flex space-x-4 mb-10">
-                    <li
-                      className="relative group items-center flex"
-                      onMouseEnter={() => setIsCollectionOpen(true)}
-                      onMouseLeave={() => setIsCollectionOpen(false)}
-                    >
+                    <li className="relative group items-center flex">
                       <Link
                         to="/collection"
                         className="text-gray-600 flex font-bold text-xl"
                       >
                         COLLECTION <ChevronDownIcon className="h-5 w-5 ml-2" />
                       </Link>
-                      {isCollectionOpen && (
-                        <div className="absolute left-0 mt-2 w-100 bg-white shadow-lg transform scale-110 transition-all duration-300 ease-out">
-                          <ul className="flex">
-                            <li>
-                              <Link
-                                to="/allcollections"
-                                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                              >
-                                All COLLECTION
-                              </Link>
-                            </li>
-                            <li>
-                              <Link
-                                to="/newcollections"
-                                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                              >
-                                NEW COLLECTIONS
-                              </Link>
-                            </li>
-                            <li>
-                              <Link
-                                to="/bestratings"
-                                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                              >
-                                BEST RATING
-                              </Link>
-                            </li>
-                          </ul>
-                        </div>
-                      )}
+                      <div className="absolute left-0 mt-2 w-100 bg-white shadow-lg opacity-0 group-hover:opacity-100 transform scale-110 group-hover:scale-100 transition-all duration-300 ease-out">
+                        <ul className="flex">
+                          <li>
+                            <Link
+                              to="/allcollections"
+                              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                            >
+                              All COLLECTION
+                            </Link>
+                          </li>
+                          <li>
+                            <Link
+                              to="/newcollections"
+                              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                            >
+                              NEW COLLECTIONS
+                            </Link>
+                          </li>
+                          <li>
+                            <Link
+                              to="/bestratings"
+                              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                            >
+                              BEST RATING
+                            </Link>
+                          </li>
+                        </ul>
+                      </div>
                     </li>
                   </ul>
                 </div>
@@ -220,7 +211,8 @@ function Navbar() {
                 <div className="flex space-x-5">
                   <div className="flex h-10 w-10 items-center text-center justify-center mt-5 text-gray-600 border-1 border-gray-400 rounded-full">
                     <Link to="/addwine" onClick={handleNavigate}>
-                      <FaPlus size={24} />
+                      {" "}
+                      <FaPlus size={24} />{" "}
                     </Link>
                   </div>
 
@@ -229,7 +221,9 @@ function Navbar() {
                       onClick={handleLogout}
                       className="absolute bg-blue-600 text-white text-xl hover:bg-blue-800 font-bold text-center h-15 w-70 flex items-center justify-center rounded transition-all ease-in-out duration-1000 hover:scale-110"
                     >
-                      Logout
+                      <p className="flex justify-center items-center text-center ">
+                        Logout
+                      </p>
                     </button>
 
                     <div className="mt-5 ml-3 bg-purple-600 hover:bg-blue-700 h-13 w-70 rounded"></div>
@@ -239,6 +233,28 @@ function Navbar() {
             </>
           )}
         </div>
+
+        {searchResults.length > 0 && (
+          <div className="search-results mt-4 bg-white shadow-lg p-4">
+            <h2 className="text-xl font-bold">Wine Collections:</h2>
+            <ul>
+              {searchResults.map((wine) => (
+                <li key={wine._id}>
+                  <Link
+                    to={`/wines/${wine._id} `}
+                    className="text-blue-600"
+                    onClick={handleClick}
+                  >
+                    {wine.wineName}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+        {text && searchResults.length === 0 && (
+          <div className="mt-4 text-gray-500">No wines found for "{text}".</div>
+        )}
       </div>
     </>
   );
